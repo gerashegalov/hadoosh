685f089127d302e292a7a2489fc85f0ad22bab4f
gerashegalov <gera@apache.org>
2013-06-07 00:39:52 -0700

    Fixed: use logging instead of println Added: relaxed hdfs completion to runjar, i.e. works even if the previous args were of some unknown type Added: info message about the default fs used Fixed: better initialize jobClient lazily as users may have a non-MR cluster. Fail only if job client commands are used

--- src/main/java/HadooSh.java
+++ src/main/java/HadooSh.java
@@ -144,7 +144,6 @@ public class HadooSh {
     localURI = FileSystem.getLocal(config).getUri();
 
     dfsShell = new FsShell(config);
-    jobClient = new JobClient(config);
 		home = fs.getWorkingDirectory();
 
 		ConsoleReader reader = new ConsoleReader();
@@ -751,7 +750,7 @@ public class HadooSh {
         ? fullCommand.substring(argIndex, fullCommand.length()).split(" ")
         : new String[0];
       System.setOut(new PrintStream(os));
-      jobClient.run(args);
+      getJobClient().run(args);
       System.out.flush();
     } catch (Throwable e) {
       if (e instanceof IOException) {
@@ -836,7 +835,7 @@ public class HadooSh {
 			final List clist)
     {
       try {
-        JobStatus[] jobs = jobClient.jobsToComplete();
+        JobStatus[] jobs = getJobClient().jobsToComplete();
         if (jobs != null) {
           String b = (buffer == null ? "" : buffer);
           for (JobStatus j : jobs) {
@@ -862,14 +861,15 @@ public class HadooSh {
 			final List clist)
     {
       try {
-        JobStatus[] jobs = jobClient.jobsToComplete();
+        final JobClient jc = getJobClient();
+        JobStatus[] jobs = jc.jobsToComplete();
         if (jobs != null) {
           String b = (buffer == null ? "" : buffer);
           for (JobStatus j : jobs) {
             final JobID jid = j.getJobID();
             final TaskReport[][] taskReports = new TaskReport[][] {
-              jobClient.getMapTaskReports(jid),
-              jobClient.getReduceTaskReports(jid)
+              jc.getMapTaskReports(jid),
+              jc.getReduceTaskReports(jid)
             };
             for (TaskReport[] reps : taskReports) {
               for (TaskReport tr : reps) {
@@ -936,4 +936,11 @@ public class HadooSh {
             avroDatastream.close();
         }
     }
+
+  private static JobClient getJobClient() throws IOException {
+    if (jobClient == null) {
+      jobClient = new JobClient(config);
+    }
+    return jobClient;
+  }
 }
